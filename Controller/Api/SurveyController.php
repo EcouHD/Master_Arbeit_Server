e6607bdd7b1594cd5fdc3fe0af3d2066bb71ed1e
Lars Eckel <eckellars@gmail.com>
2024-04-18 13:30:23 +0200

    feat: POST Request to server works but has no functionality yet

--- Controller/Api/SurveyController.php
+++ Controller/Api/SurveyController.php
@@ -137,5 +137,51 @@ class SurveyController extends BaseController
 	}
 
     }
+
+    /**
+     * "/survey/sendQuestionResult" Endpoint - receive data of user
+     * and calculate final answer with the help of the gazedata
+     */
+    public function sendQuestionResultAction() {
+
+	$strErrorDesc = '';
+
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+
+	if(strtoupper($requestMethod) == 'POST') {
+		try{
+
+			$input = json_decode(file_get_contents('php://input'), true);
+			// not sure if you can get the data here like this because we called this php file from the input.php
+			// we need to execute SELECT statement for getting the UI positions of the user
+			// followed by putting the gazedata into context --> which answer was looked at the most
+
+                } catch (Error $e) {
+
+                        $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+
+                        $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+                }
+
+	}
+
+	// send output
+        
+        if(!$strErrorDesc) {
+                                       
+                $this->sendOutput(
+                        json_encode(array('message' => 'Testing if POST works')), 
+                        array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+                );
+        } else {
+
+                $this->sendOutput(json_encode(array('error' => $strErrorDesc)),
+                        array('Content-Type: application/json', $strErrorHeader)
+                );
+        }
+
+    }
+
+
 }
 
