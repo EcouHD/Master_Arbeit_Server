8d330555d008e141f67a8135fea1072725d1e102
Lars Eckel <eckellars@gmail.com>
2024-05-31 15:37:47 +0200

    refactor: change pw on database in aws

--- inc/config.php
+++ inc/config.php
@@ -4,7 +4,7 @@ define("DB_HOST", "localhost");
 
 define("DB_USERNAME", "gazeUser");
 
-define("DB_PASSWORD", "@Christian93");
+define("DB_PASSWORD", "uni@ulm/Master2024");
 
 define("DB_DATABASE_NAME", "gaze_database");
 
---
+++ test.php
@@ -0,0 +1,3 @@
+<?php
+
+echo json_encode("Welcome to my website! There is nothing to see, so go on and continue with your journey and have a great day ;)");
